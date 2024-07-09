f1fa965137f488a1f83cad387eb2d1a382b5a979
TEJASAVE BISEN <tejasavebisen@gmail.com>
2024-07-09 18:45:31 +0530

    var

---
+++ var.js
@@ -0,0 +1,12 @@
+var b ="we are good";
+var a=10;
+console.log(b);
+console.log(a);
+{
+ let   s=10;
+   const sum = 30;
+   let d=40;
+    console.log(a+b+d);
+    console.log(sum);
+}
+console.log(b+a);
